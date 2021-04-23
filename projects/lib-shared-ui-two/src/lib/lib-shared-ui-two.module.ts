afecf441f3813d0ebadc5ecc51d3628c172304ef
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-23 19:04:40 +0200

    widget & widget header code changes

--- projects/lib-shared-ui-two/src/lib/lib-shared-ui-two.module.ts
+++ projects/lib-shared-ui-two/src/lib/lib-shared-ui-two.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { LibSharedUiTwoComponent } from './lib-shared-ui-two.component';
-import { WidgetComponent } from './widget/widget.component';
-import { WidgetHeaderComponent } from './widget-header/widget-header.component';
-
-
+import { NgModule } from "@angular/core";
+import { LibSharedUiTwoComponent } from "./lib-shared-ui-two.component";
+import { WidgetComponent } from "./widget/widget.component";
+import { WidgetHeaderComponent } from "./widget-header/widget-header.component";
 
 @NgModule({
-  declarations: [LibSharedUiTwoComponent, WidgetComponent, WidgetHeaderComponent],
-  imports: [
+  declarations: [
+    LibSharedUiTwoComponent,
+    WidgetComponent,
+    WidgetHeaderComponent,
   ],
-  exports: [LibSharedUiTwoComponent]
+  imports: [],
+  exports: [LibSharedUiTwoComponent, WidgetComponent, WidgetHeaderComponent],
 })
-export class LibSharedUiTwoModule { }
+export class LibSharedUiTwoModule {}
