2529ab3a01b31a2b4b74f8177ac005b587c8576f
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-24 17:16:13 +0200

    const used as token

--- projects/demo-app-two/src/app/app.module.ts
+++ projects/demo-app-two/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LibSharedUiTwoModule } from "./../../../lib-shared-ui-two/src/lib/lib-shared-ui-two.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LibSharedUiTwoModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects/lib-shared-ui-two/src/lib/widget-header/widget-header.component.ts
+++ projects/lib-shared-ui-two/src/lib/widget-header/widget-header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, InjectionToken, OnInit } from "@angular/core";
+
+export const Header_Token = new InjectionToken<WidgetHeaderComponent>(
+  "widget-header"
+);
 
 @Component({
-  selector: 'lib-widget-header',
-  templateUrl: './widget-header.component.html',
-  styleUrls: ['./widget-header.component.scss']
+  selector: "lib-widget-header",
+  templateUrl: "./widget-header.component.html",
+  styleUrls: ["./widget-header.component.scss"],
+  providers: [
+    {
+      provide: Header_Token,
+      useExisting: WidgetHeaderComponent,
+    },
+  ],
 })
 export class WidgetHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
